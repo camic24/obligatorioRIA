fa5e0f31ff369580a26ad60e3e754cff10169fd3
Camila Cardozo <camili2409@hotmail.com>
2022-06-05 23:09:21 -0300

    first commit to heroku

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { SearchComponent } from './pages/search/search.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { VideoplayerComponent } from './videoplayer/videoplayer.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/home"
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+
+  },
+  {
+    path: "search",
+    children: [
+      {
+        path: "names/:q",
+        component: SearchComponent,
+        data:{
+          type:"name",
+          //image:"assets/ck.svg"
+        }
+      },
+      {
+        path: "ingredients/:q",
+        component: SearchComponent,
+        data:{
+          type:"ing",
+          //image:"assets/apple.svg"
+        }
+      },
+      {
+        path: "category/:q",
+        component: SearchComponent,
+        data:{
+          type:"category",
+          //image:"assets/coco.svg"
+        }
+      }
+    ],
+  },
+  {path: '**', redirectTo: '/home'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
