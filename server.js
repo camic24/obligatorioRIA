bb1130e8a806e57357f141ab10bca20273042c48
Camila Cardozo <camili2409@hotmail.com>
2022-06-06 13:19:06 -0300

    prueba

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/obligatorioria'));
 
-app.get('*', function (req, res) {
-    res.sendFile('index.html');
+if (process.env.NODE_ENV) {
+    //static folder add
+  app.use(express.static('app/client/build'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname , "app/client/build", "index.html"));
   });
+  }
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
